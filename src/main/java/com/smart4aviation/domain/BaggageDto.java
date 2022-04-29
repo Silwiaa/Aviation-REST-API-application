b9c46ae1c56ddff59f0dcd0241b3f8a6558d4468
Sylwia Krzymowska <s.krzymowska@gmail.com>
2022-04-29 13:45:33 +0200

    BaggageDto added

---
+++ src/main/java/com/smart4aviation/domain/BaggageDto.java
@@ -0,0 +1,29 @@
+package com.smart4aviation.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class BaggageDto {
+    @JsonProperty("id")
+    private long id;
+
+    @JsonProperty("totalWeight")
+    private double weight;
+
+    @JsonProperty("weightUnit")
+    private WeightUnit weightUnit;
+
+    @JsonProperty("pieces")
+    private int pieces;
+
+    private long cargoEntityId;
+}
