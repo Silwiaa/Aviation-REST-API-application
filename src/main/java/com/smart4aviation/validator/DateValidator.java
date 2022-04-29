3e152557581959e0be1596b445e90dbfefb30b7a
Sylwia Krzymowska <s.krzymowska@gmail.com>
2022-04-29 15:28:35 +0200

    Bug in DateValidator fixed

--- src/main/java/com/smart4aviation/validator/DateValidator.java
+++ src/main/java/com/smart4aviation/validator/DateValidator.java
@@ -13,13 +13,21 @@ public class DateValidator {
 
     public String validateDate(final String date) throws InvalidDateException {
         String delimiter = "-";
+        if (date.length() != 10) {
+            LOGGER.error("Invalid date");
+            throw new InvalidDateException();
+        }
+
+        validateString(date.substring(0, 4));
+        validateString(date.substring(5, 7));
+        validateString(date.substring(8, 10));
+
         int year = Integer.parseInt(date.substring(0, 4));
         int month = Integer.parseInt(date.substring(5, 7));
         int day = Integer.parseInt(date.substring(8, 10));
         LocalDate localDate = LocalDate.of(year, month, day);
 
         if (
-                date.length() != 10 ||
                 !date.substring(4, 5).equals(delimiter) ||
                 !date.substring(7, 8).equals(delimiter) ||
                 localDate.isBefore(LocalDate.of(1903, 1, 1)) ||
@@ -29,5 +37,14 @@ public class DateValidator {
             throw new InvalidDateException();
         }
         return date;
+    }
+
+    private void validateString(String validatedString) throws InvalidDateException {
+        for (char c : validatedString.toCharArray()) {
+            if (!Character.isDigit(c)) {
+                LOGGER.error("Invalid date");
+                throw new InvalidDateException();
+            }
+        }
     }
 }
