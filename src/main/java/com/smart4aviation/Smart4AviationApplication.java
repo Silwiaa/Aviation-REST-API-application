b091aa8c01d6692bb9407eb093672857156112eb
Sylwia Krzymowska <s.krzymowska@gmail.com>
2022-04-29 14:33:08 +0200

    RecruitmentTask project done

---
+++ src/main/java/com/smart4aviation/Smart4AviationApplication.java
@@ -0,0 +1,11 @@
+package com.smart4aviation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Smart4AviationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Smart4AviationApplication.class, args);
+    }
+}
---
+++ src/test/java/com/smart4aviation/Smart4AviationApplicationTests.java
@@ -0,0 +1,12 @@
+package com.smart4aviation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Smart4AviationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
